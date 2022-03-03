198057649ed39dbc57f0963f0d47bf8ccbfe15df
Simon <simon@indicio.tech>
2022-03-03 14:18:32 -0700

    feat: adding OOB invitations
    Signed-off-by: Simon <simon@indicio.tech>

--- src/UI/Home.js
+++ src/UI/Home.js
@@ -1,26 +1,25 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState } from 'react'
 
 import styled from 'styled-components'
 
-import { handleGovernance } from './util'
-
 import FormQR from './FormQR'
-import { useNotification } from './NotificationProvider'
+import FormInvitationAccept from './FormInvitationAccept'
+// import { useNotification } from './NotificationProvider'
 
 import { CanUser } from './CanUser'
 
-const HeaderHolder = styled.div`
+const DashboardRow = styled.div`
   display: flex;
   justify-content: space-between;
 `
 
-const ContentFlexBox = styled.div`
-  width: 32%;
+const DashboardButton = styled.div`
+  width: 30%;
   min-width: 240px;
   height: 150px;
-  margin-bottom: 30px;
+  margin: 30px 10px 0px 0px;
   padding: 0 25px;
-  font-size: calc(12px + 1.5vw);
+  font-size: calc(12px + 1.0vw);
   line-height: 150px;
   vertical-align: center;
   text-transform: uppercase;
@@ -36,86 +35,96 @@ const ContentFlexBox = styled.div`
   }
 `
 
+// const DashboardPlaceholder = styled.div`
+//   width: 32%;
+//   min-width: 240px;
+//   height: 150px;
+//   margin-bottom: 30px;
+//   padding: 0 25px;
+// `
+
 function Home(props) {
-  const error = props.errorMessage
-  const success = props.successMessage
-  const warning = props.warningMessage
   const localUser = props.loggedInUserState
-  const privileges = props.privileges
-  // const actions = props.actions
-  // const participants = props.participants
-
-  const [govGranted, setGovGranted] = useState(undefined)
-
-  const [index, setIndex] = useState(false)
-  const [contactModalIsOpen, setContactModalIsOpen] = useState(false)
-  const [credentialModalIsOpen, setCredentialModalIsOpen] = useState(false)
-
-  const isMounted = useRef(null)
 
   // Accessing notification context
-  const setNotification = useNotification()
+  // const setNotification = useNotification()
 
-  // Handle governance
-  // useEffect(() => {
-  //   setGovGranted(handleGovernance(privileges, actions, participants))
-  // }, [privileges, actions, participants])
+  const [oob, setOOB] = useState(false)
 
-  console.log(privileges)
+  const [scanModalIsOpen, setScanModalIsOpen] = useState(false)
+  const [displayModalIsOpen, setDisplayModalIsOpen] = useState(false)
 
-  useEffect(() => {
-    if (success) {
-      setNotification(success, 'notice')
-      props.clearResponseState()
-    } else if (error) {
-      setNotification(error, 'error')
-      props.clearResponseState()
-      setIndex(index + 1)
-    } else if (warning) {
-      setNotification(warning, 'warning')
-      props.clearResponseState()
-      setIndex(index + 1)
-    } else return
-  }, [error, success, warning])
+  const closeScanModal = () => setScanModalIsOpen(false)
+  const closeDisplayModal = () => setDisplayModalIsOpen(false)
 
-  // Get governance privileges
-  useEffect(() => {
-    isMounted.current = true
-    props.sendRequest('GOVERNANCE', 'GET_PRIVILEGES', {})
-    // props.sendRequest('GOVERNANCE', 'GET_PARTICIPANTS', {})
-    // props.sendRequest('GOVERNANCE', 'GET_ACTIONS', {})
-    return () => {
-      isMounted.current = false
-    }
-  }, [])
+  const scanInvite = (type) => {
+    type === 'oob' ? setOOB(true) : setOOB(false)
 
-  const closeContactModal = () => setContactModalIsOpen(false)
-  const closeCredentialModal = () => setCredentialModalIsOpen(false)
+    setScanModalIsOpen((o) => !o)
+  }
 
-  const addContact = () => {
-    // // Simple use of governance
-    if (privileges && privileges.includes('verify_identity')) {
-      setContactModalIsOpen((o) => !o)
-      props.sendRequest('INVITATIONS', 'CREATE_SINGLE_USE', {})
-    } else
-      setNotification("Error: you don't have the right privileges", 'error')
+  const presentOutOfBand = () => {
+    setDisplayModalIsOpen((o) => !o)
+    props.sendRequest('OUT_OF_BAND', 'CREATE_INVITATION', {})
+  }
+
+  const presentInvitation = () => {
+    setDisplayModalIsOpen((o) => !o)
+    props.sendRequest('INVITATIONS', 'CREATE_SINGLE_USE', {})
   }
 
   return (
     <>
-      <HeaderHolder>
+      <DashboardRow>
         <CanUser
           user={localUser}
           perform="contacts:create"
           yes={() => (
-            <ContentFlexBox onClick={addContact}>Add Contact</ContentFlexBox>
+            <DashboardButton onClick={() => scanInvite('connection')}>
+              Scan QR Code
+            </DashboardButton>
           )}
         />
-      </HeaderHolder>
+        <CanUser
+          user={localUser}
+          perform="contacts:create"
+          yes={() => (
+            <DashboardButton onClick={presentInvitation}>
+              Display QR Code
+            </DashboardButton>
+          )}
+        />
+        <CanUser
+          user={localUser}
+          perform="contacts:create"
+          yes={() => (
+            <DashboardButton onClick={() => scanInvite('oob')}>
+              Scan OOB
+            </DashboardButton>
+          )}
+        />
+        <CanUser
+          user={localUser}
+          perform="contacts:create"
+          yes={() => (
+            <DashboardButton onClick={presentOutOfBand}>
+              Display OOB
+            </DashboardButton>
+          )}
+        />
+        {/* <DashboardPlaceholder></DashboardPlaceholder> */}
+      </DashboardRow>
+      <FormInvitationAccept
+        oob={oob}
+        contactModalIsOpen={scanModalIsOpen}
+        closeContactModal={closeScanModal}
+        sendRequest={props.sendRequest}
+      />
       <FormQR
-        contactModalIsOpen={contactModalIsOpen}
-        closeContactModal={closeContactModal}
+        contactModalIsOpen={displayModalIsOpen}
+        closeContactModal={closeDisplayModal}
         QRCodeURL={props.QRCodeURL}
+        sendRequest={props.sendRequest}
       />
     </>
   )
